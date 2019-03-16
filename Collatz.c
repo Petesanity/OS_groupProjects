807caa0b0c6fa7240b3f123299442b4df06273c2
Petesanity <peterjr05@yahoo.com>
2019-03-16 12:21:20 -0400

    Collatz

--- Collatz.c
+++ Collatz.c
@@ -10,48 +10,49 @@ Group Names:
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <cstdlib>
 
-int main(){
-
-	printf("Enter a number N :"); //prompt user for number
-	int n;
-	scanf("%d", &n); //user input for number n
-
-	while(n<=0){ //error checking so a positive integer is passed
-		printf("Please enter a positive number");
-		scanf("%d", &n); //user input for number n
-	}
-	int returnValue = fork();  //make 2 processes which run the same program after this instruction
+int main(int argc, char *argv[]){
+	int arr = atoi(argv[1]); //convert char array and store it in int variable
 	
-	if(returnValue == 0){ //only child process executes whats inside this statement
+	if(argc >=2){ // check if command line arguments are at least 2, one for ./Collatz and one for user input
+		
+		int returnValue = fork();  //make 2 processes which run the same program after this instruction
 
-		while(n!=1){
-			printf("Child executing\n");
-			printf("%d\n",  n);
-			if(n%2 == 0){ //case for even
+		if(returnValue == 0){ //only child process executes whats inside this statement
 
-				n = n/2;
+			while(arr!=1 && arr>0){ //if input is not 1
+				printf("Child executing\n"); //printing initial number
+				printf("%d\n", arr);
+
+				if(arr%2 == 0){ //case for even
+
+					arr= arr/2;
+
+				}
+
+				else if(arr%2==1){//case for odd		
+
+					arr = (3 *arr) + 1;
+
+				}
+
+				if(arr == 1){ //printing one at the end of calculations
+					printf("%d",arr);
+					printf("\n");
+				}
+			}
+			if(arr <=0){ //error checking to make sure user inputs a postive input value
+				printf("PLEASE ENTER POSITIVE INPUT VALUE IN COMMAND LINE \n" );
 
 			}
-
-			else if(n%2==1){//case for odd		
-
-				n = (3 *n) + 1;
-
-			}
-
-			if(n == 1){
-				printf("Child executing\n");
-				printf("%d  ",n);
-				printf("\n");
-				printf("Child exiting\n");
-			}
-		}
+		}		
 	}
 	printf("Parent waiting\n");
-	wait(NULL);
+	wait(NULL); //parent involking wait call
 	printf("Parent exiting\n");
-
 	return 0;		
 }
